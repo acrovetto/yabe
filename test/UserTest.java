b2477b578c3dc562e6595ef4da22e892a53a32df
acrovetto <acrovetto@PC080505.axones.fr>
2011-03-11 16:23:37 +0100

    Model and Test

---
+++ test/UserTest.java
@@ -0,0 +1,47 @@
+import models.User;
+
+import org.junit.Before;
+import org.junit.Test;
+
+import play.test.Fixtures;
+import play.test.UnitTest;
+
+public class UserTest extends UnitTest {
+
+    @Before
+    public void setup() {
+        Fixtures.deleteAll();
+    }
+
+    @Test
+    public void createAndRetrieveUser() {
+        // Create a new user and save it
+        new User("bob@gmail.com", "secret", "Bob").save();
+
+        // Retrieve the user with e-mail address bob@gmail.com
+        User bob = User.find("byEmail", "bob@gmail.com").first();
+
+        // Test
+        assertNotNull(bob);
+        assertEquals("Bob", bob.fullname);
+    }
+
+    @Test
+    public void connectTest() {
+        String email = "toto@titi.fr";
+        String password = "123456";
+        String name = "Toto";
+
+        User user = User.connect(email, password);
+        assertNull(user);
+
+        // Create a new user and save it
+        new User(email, password, name).save();
+
+        user = User.connect(email, password);
+        assertNotNull(user);
+
+        user.delete();
+    }
+
+}
