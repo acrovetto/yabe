15b24f6426be1e565b2d2fbd0a4ffd1dc5057dd5
acrovetto <acrovetto@PC080505.axones.fr>
2011-03-11 17:20:34 +0100

    add post

--- app/controllers/Application.java
+++ app/controllers/Application.java
@@ -1,6 +1,7 @@
 package controllers;
 
 import play.*;
+import play.data.validation.Required;
 import play.mvc.*;
 
 import java.util.*;
@@ -9,8 +10,33 @@ import models.*;
 
 public class Application extends Controller {
 
-    public static void index() {
-        render();
+    @Before
+    static void addDefaults() {
+        renderArgs.put("blogTitle", Play.configuration.getProperty("blog.title"));
+        renderArgs.put("blogBaseline", Play.configuration.getProperty("blog.baseline"));
     }
 
+    public static void index() {
+        Post frontPost = Post.find("order by postedAt desc").first();
+        List<Post> olderPosts = Post.find("order by postedAt desc").from(1).fetch(10);
+        render(frontPost, olderPosts);
+    }
+
+    public static void show(Long id) {
+        Post post = Post.findById(id);
+        render(post);
+    }
+
+    public static void postComment(Long postId, @Required String author, @Required String content) {
+        Post post = Post.findById(postId);
+        if (validation.hasErrors()) {
+            render("Application/show.html", post);
+        }
+
+        post.addComment(author, content);
+        
+        flash.success("Thanks for posting %s", author);
+        
+        show(postId);
+    }
 }
