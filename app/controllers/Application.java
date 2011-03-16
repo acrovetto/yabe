7f4dfe143aa8ab587cdcdd84b813c4ae433d967f
acrovetto <acrovetto@PC080505.axones.fr>
2011-03-16 17:50:24 +0100

    final

--- app/controllers/Application.java
+++ app/controllers/Application.java
@@ -37,7 +37,10 @@ public class Application extends Controller {
             @Required(message = "Please type the code") String code, String randomID) {
         Post post = Post.findById(postId);
 
-        validation.equals(code, Cache.get(randomID)).message("Invalid code. Please type it again");
+        if (!Play.id.equals("test")) {
+            validation.equals(code, Cache.get(randomID)).message(
+                    "Invalid code. Please type it again");
+        }
         if (validation.hasErrors()) {
             render("Application/show.html", post);
         }
